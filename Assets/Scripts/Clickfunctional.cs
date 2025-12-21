ed296ef7a96389937a79d57a32949a3176fac5dd
hovakimyanerik2006 <hovakimyane95@gmail.com>
2025-12-21 15:33:53 +0400

    .

--- Assets/Scripts/Clickfunctional.cs
+++ Assets/Scripts/Clickfunctional.cs
@@ -4,24 +4,7 @@ using UnityEngine.InputSystem;
 
 public class Clickfunctional : MonoBehaviour
 {
-    //public float jumpForce = 20f;
-    //private Rigidbody2D rb;
-
-    //void Start()
-    //{
-    //    rb = GetComponent<Rigidbody2D>();
-    //}
-
-    //void Update()
-    //{
-    //    if (Mouse.current.leftButton.wasPressedThisFrame)
-    //    {
-    //        rb.linearVelocity = Vector2.zero;
-    //        Vector2 dir = new Vector2(1, 1).normalized;
-    //        rb.AddForce(dir * jumpForce, ForceMode2D.Impulse);
-    //    }
-    //}
-    public float speed = 50f;
+    public float speed = 3f;
     private Rigidbody2D rb;
 
     void Start()
@@ -32,20 +15,17 @@ public class Clickfunctional : MonoBehaviour
     void FixedUpdate()
     {
         Vector2 direction;
-
-        // Եթե հպում կա (touch կամ mouse)
         if (Input.GetMouseButton(0))
         {
-            // 45° վերև
+
             direction = new Vector2(1f, 1f).normalized;
         }
         else
         {
-            // 45° ներքև
+
             direction = new Vector2(1f, -1f).normalized;
         }
-
-        // Շարժում Rigidbody-ով
+      
         rb.linearVelocity = direction * speed;
         bool isPressed =
             (Touchscreen.current != null && Touchscreen.current.primaryTouch.press.isPressed) ||
@@ -58,7 +38,7 @@ public class Clickfunctional : MonoBehaviour
 
         rb.linearVelocity = direction * speed;
 
-        // ⬇️ ՍԼԱՔԻ ՊՏՏՈՒՄԸ ՇԱՐԺՄԱՆ ՈՒՂՂՈՒԹՅԱՄԲ
+      
         if (rb.linearVelocity.sqrMagnitude > 0.001f)
         {
             float angle = Mathf.Atan2(rb.linearVelocity.y, rb.linearVelocity.x) * Mathf.Rad2Deg;
