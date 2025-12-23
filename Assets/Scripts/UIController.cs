03b4770cc5d9d7058579b7b35585473fed793135
Mandarin658 <manegevorgyan557@gmail.com>
2025-12-23 19:58:09 +0400

    button,basic ui

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,22 @@
+﻿using System.Net.NetworkInformation;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ClickAction : MonoBehaviour
+{
+    void Update()
+    {
+        // Mouse ձախ քլիկ
+        if (Input.GetMouseButtonDown(0))
+        {
+            DoAction();
+        }
+    }
+
+    // Նույն ֆունկցիան կկանչվի նաև Button-ից
+    public void DoAction()
+    {
+        Debug.Log("Գործառույթը կատարվեց");
+
+    }
+}
