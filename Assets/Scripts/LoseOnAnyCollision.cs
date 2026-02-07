d35bc5ba06a488c2fa352fc7eb600fa30170b6d3
Mandarin658 <manegevorgyan557@gmail.com>
2026-02-07 22:47:17 +0400

    Win canvas

--- Assets/Scripts/LoseOnAnyCollision.cs
+++ Assets/Scripts/LoseOnAnyCollision.cs
@@ -1,22 +1,39 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class LoseOnAnyCollision : MonoBehaviour
 {
     public GameObject gameOverCanvas;
+    public GameObject winCanvas;
+
+    public string safeTag = "Safe"; // tag, որի հետ բախվելիս GameOver չի լինի
 
     void Start()
     {
         gameOverCanvas.SetActive(false);
+        winCanvas.SetActive(false);
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        GameOver();
+        // Եթե բախված օբյեկտը ՉՈՒՆԻ safeTag
+        if (!collision.gameObject.CompareTag(safeTag))
+        {
+            GameOver();
+        }
+        else
+        {
+            WinCanvas();
+        }
     }
 
     void GameOver()
     {
         gameOverCanvas.SetActive(true);
+        Time.timeScale = 0f;
+    }
+    void WinCanvas()
+    {
+        winCanvas.SetActive(true);
         Time.timeScale = 0f;
     }
 }
