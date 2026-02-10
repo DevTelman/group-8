53e932f5538161d71a931c7fc5c911fdc76cdfe8
Mandarin658 <manegevorgyan557@gmail.com>
2026-02-11 00:39:03 +0400

    stars

---
+++ Assets/Scripts/Star.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Star : MonoBehaviour
+{
+    private StarManager starManager;
+    private bool collected = false;
+
+    void Start()
+    {
+        starManager = FindObjectOfType<StarManager>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (collected) return;
+
+        if (other.transform.root.CompareTag("Player"))
+        {
+            collected = true;
+            starManager.CollectStar();
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/StarManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class StarManager : MonoBehaviour
+{
+    public Image[] starIcons; // 3 UI աստղերը
+    private int collectedStars = 0;
+
+    void Start()
+    {
+        // Սկզբում բոլորը անջատված
+        foreach (Image img in starIcons)
+        {
+            img.enabled = false;
+        }
+    }
+
+    public void CollectStar()
+    {
+        if (collectedStars >= starIcons.Length) return;
+
+        starIcons[collectedStars].enabled = true; // UI աստղը երևում է
+        collectedStars++;
+    }
+}
