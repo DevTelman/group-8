342a81b63bc76c4cd217d5e6d70add2f9e79f589
Mandarin658 <manegevorgyan557@gmail.com>
2026-02-11 01:16:52 +0400

    TrashController

---
+++ Assets/Scripts/TrashController.cs
@@ -0,0 +1,28 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class ArrowController : MonoBehaviour
+{
+    public int collectedStars = 0;
+    public TMP_Text starsText;
+
+    private void Start()
+    {
+        UpdateStarsUI();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Garbage"))
+        {
+            collectedStars++;
+            UpdateStarsUI();
+            Destroy(collision.gameObject);
+        }
+    }
+
+    void UpdateStarsUI()
+    {
+        starsText.text =""+ collectedStars;
+    }
+}
