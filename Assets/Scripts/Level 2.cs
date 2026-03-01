600f242e5aa72a2499bd22561a2e5a8cd68e05c9
Mandarin658 <manegevorgyan557@gmail.com>
2026-03-01 19:38:58 +0400

    33

---
+++ Assets/Scripts/Level 2.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class level2 : MonoBehaviour
+{
+    public void GoToTLevel2Scene()
+    {
+        SceneManager.LoadScene(3);
+    }
+}
