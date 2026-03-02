2d9cd4c2ab816827a43c31f3a29751c52e787d9f
Mandarin658 <manegevorgyan557@gmail.com>
2026-03-02 16:32:08 +0400

    boss level

---
+++ Assets/Scenes/THE HUB/BloomFlash.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+using static WinZone;
+
+public class BloomFlash : MonoBehaviour
+{
+    public Volume volume;
+    public float maxBloom = 6f;
+    public float flashDuration = 1f;
+
+    Bloom bloom;
+
+    void Start()
+    {
+        volume.profile.TryGet(out bloom);
+
+        if (WinManager.levelCompleted)
+        {
+            StartCoroutine(Flash());
+            WinManager.levelCompleted = false; // reset
+        }
+    }
+
+    IEnumerator Flash()
+    {
+        float time = 0;
+
+        while (time < flashDuration / 2)
+        {
+            time += Time.deltaTime;
+            bloom.intensity.value = Mathf.Lerp(0, maxBloom, time / (flashDuration / 2));
+            yield return null;
+        }
+
+        time = 0;
+
+        while (time < flashDuration / 2)
+        {
+            time += Time.deltaTime;
+            bloom.intensity.value = Mathf.Lerp(maxBloom, 0, time / (flashDuration / 2));
+            yield return null;
+        }
+
+        bloom.intensity.value = 0;
+    }
+}
